36055aa5a6cdc3271fa03aab834c0eee329e157e
tekmbucket <thomasekmorris@gmail.com>
2018-08-01 23:11:39 +1000

    Updated console default keymap to reflect the keyboard layout editor layout. ALso update C and H file names.

--- keyboards/fortysix_console/keymaps/default/keymap.c
+++ keyboards/fortysix_console/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-#include "fortysix.h"
+#include "fortysix_console.h"
 #include "action_layer.h"
 #include "quantum.h"
 #include "action.h"
@@ -10,8 +10,8 @@
 
 enum fortysix_layers {
   _QWERTY,
-  _LAYRGB,
-  _LRESET
+  _FUNCTION,
+  _LAYRGB
 };
 
 
@@ -23,35 +23,67 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ------------------------------------
- * |   1  |   2  |   3  |   4  |   5  |
+ * |  ESC | PTSC | PAUS |  DEL |  WIN |
  * |------+------+------+------+------|
- * |   Q  |   W  |   E  |   R  |   T  |
+ * |  FN  |   7  |   8  |   9  |   /  |
  * |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |
+ * | CTRL |   4  |   5  |   6  |   *  |
  * |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |
+ * |  ALT |   1  |   2  |   3  |   -  |
  * |------+------+------+------+------|
- * |   1  |   2  |   3  |   4  |   5  |
+ * |  TAB |   `  |   0  |   .  |   +  |
  * |------+------+------+------+------|
- * |   Q  |   W  |   E  |   R  |   T  |
+ * |   ;  |   [  |   ]  |   \  |  -_  |
  * |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |
+ * | HOME |  END |  UP  | PGUP | PGDN |
  * |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |
+ * |      | LEFT | DOWN | RGHT |      |
  * ------------------------------------
  * Thumb cluster is displayed as a signle row running clockwise from top left.
  * Cluster can, of course, be physically rotated so check your keyboard.
  */
 
 	[_QWERTY] = LAYOUT_fortysix_half(
-		KC_1, KC_2, KC_3, KC_4, KC_5,
-		KC_Q, KC_W, KC_E, KC_R, KC_T,
-		KC_A, KC_S, KC_D, KC_F, KC_G,
-		KC_Z, KC_X, KC_C, KC_V, KC_B,
-		KC_1, KC_2, KC_3, KC_4, KC_5,
-		KC_Q, KC_W, KC_E, KC_R, KC_T,
-		KC_A, KC_S, KC_D, KC_F, KC_G,
-		KC_Z, KC_X, KC_C, KC_V, KC_B),
+		KC_ESC, KC_PSCR, KC_PAUS, KC_DEL, KC_APP,
+		KC_, KC_P7, KC_P8, KC_P9, KC_PSLS,
+		KC_RCTRL, KC_P4, KC_P5, KC_P6, KC_PAST,
+		KC_RALT, KC_P1, KC_P2, KC_P3, KC_PMNS,
+		KC_TAB, KC_GRAVE, KC_P0, KC_PDOT, KC_PPLS,
+		KC_SCOLON, KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_MINUS,
+		KC_HOME, KC_END, KC_UP, KC_PGUP, KC_PGDN,
+		KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO),
+
+/* FUNCTION
+ * ------------------------------------
+ * |  ESC |  F10 |  F11 |  F12 |  WIN |
+ * |------+------+------+------+------|
+ * |  FN  |  F7  |  F8  |  F9  |      |
+ * |------+------+------+------+------|
+ * | CTRL |  F4  |  F5  |  F6  |      |
+ * |------+------+------+------+------|
+ * |  ALT |  F1  |  F2  |  F3  |      |
+ * |------+------+------+------+------|
+ * |  TAB |      |      |      |      |
+ * |------+------+------+------+------|
+ * |      |      |      |      |      |
+ * |------+------+------+------+------|
+ * |      |      | PLAY |      |      |
+ * |------+------+------+------+------|
+ * |      |  |<< | STOP |  >>| |      |
+ * ------------------------------------
+ * Thumb cluster is displayed as a signle row running clockwise from top left.
+ * Cluster can, of course, be physically rotated so check your keyboard.
+ */
+
+	[_FUNCTION] = LAYOUT_fortysix_half(
+    KC_ESC, KC_F10, KC_F11, KC_F12, KC_APP,
+		KC_TRNS, KC_F7, KC_F8, KC_F9, KC_NO,
+		KC_RCTRL, KC_F4, KC_F5, KC_F6, KC_NO,
+		KC_RALT, KC_F1, KC_F2, KC_F3, KC_NO,
+		KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO,
+		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		KC_NO, KC_NO, KC_MPLY, KC_NO, KC_NO,
+		KC_NO, KC_MPRV, KC_MSTP, KC_MNXT, KC_NO),
 
 /* LAYRGB
  * ------------------------------------
@@ -69,53 +101,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|
  * |   A  |   S  |   D  |   F  |   G  |
  * |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |
+ * |   Z  |   X  | RSET |   V  |   B  |
  * ------------------------------------
  * Thumb cluster is displayed as a signle row running clockwise from top left.
  * Cluster can, of course, be physically rotated so check your keyboard.
  */
 
 	[_LAYRGB] = LAYOUT_fortysix_half(
-        KC_NO, RGB_RMOD, RGB_TOG, RGB_MOD, KC_NO,
+    KC_NO, RGB_RMOD, RGB_TOG, RGB_MOD, KC_NO,
 		KC_NO, RGB_HUD, KC_NO, RGB_HUI, KC_NO,
 		KC_NO, RGB_SAD, RGB_M_P, RGB_SAI, KC_NO,
 		KC_NO, RGB_VAD, RGB_M_T, RGB_VAI, KC_NO,
 		KC_1, KC_2, KC_3, KC_4, KC_5,
 		KC_Q, KC_W, KC_E, KC_R, KC_T,
 		KC_A, KC_S, KC_D, KC_F, KC_G,
-		KC_Z, KC_X, KC_C, KC_V, KC_B),
-
-/* LAYRGB
- * ------------------------------------
- * |   1  |   2  |   3  |   4  |   5  |
- * |------+------+------+------+------|
- * |   Q  |   W  |   E  |   R  |   T  |
- * |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |
- * |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |
- * |------+------+------+------+------|
- * |   1  |   2  |   3  |   4  |   5  |
- * |------+------+------+------+------|
- * |   Q  |   W  |   E  |   R  |   T  |
- * |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |
- * |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |
- * ------------------------------------
- * Thumb cluster is displayed as a signle row running clockwise from top left.
- * Cluster can, of course, be physically rotated so check your keyboard.
- */
-
-	[_LAYRGB] = LAYOUT_fortysix_half(
-        KC_NO, RGB_RMOD, RGB_TOG, RGB_MOD, KC_NO,
-		KC_NO, RGB_HUD, KC_NO, RGB_HUI, KC_NO,
-		KC_NO, RGB_SAD, RGB_M_P, RGB_SAI, KC_NO,
-		KC_NO, RGB_VAD, RGB_M_T, RGB_VAI, KC_NO,
-		KC_1, KC_2, KC_3, KC_4, KC_5,
-		KC_Q, KC_W, KC_E, KC_R, KC_T,
-		KC_A, KC_S, KC_D, KC_F, KC_G,
-		KC_Z, KC_X, KC_C, KC_V, KC_B),
+		KC_Z, KC_X, RESET, KC_V, KC_B),
 
 
 };
--- keyboards/fortysix_console/readme.md
+++ keyboards/fortysix_console/readme.md
@@ -1,13 +1,13 @@
-FortySix
+FortySix Console
 ========
 
 ![FortySix]()
-
+The FortySix Console is an add-on board for the FortySix, it adds a large grid of numpad and other control keys (esc, ctrl, del end along with other brackets and the arrow keys). The Console is a 38-key board or optionally 40 key if the RGB LEDs are not installed.
 The FortySix is a split 46-key column staggered keyboard with 3-key thumb clusters (2x 23 key boards) made by Frienderman it runs off a Sparkfun Pro micro using a 5*8 Matrix (4*5 for the main boards and 1*3 for the clusters). Each hand (lefty & righty) is run off an individual pro micro and can be layer switched individually.
 
 - Keyboard maintainer: [Frienderman](http://github.com/frienderman)  
 - Hardware supported:
-    - FortySix V1.0  
+    - FortySix Console V1.0  
 - Hardware availability:  
     - Not current available.  
 
@@ -17,8 +17,8 @@ The FortySix is a split 46-key column staggered keyboard with 3-key thumb cluste
 
 In the root directory of the repository, build the firmware with a command like (using lefty or righty as needed):
 
-    make fortysix:<keymap>
+    make fortysix_console:<keymap>
 
-    e.g. make fortysix:default_lefty would build the left hand default keymap.
+    e.g. make fortysix_console:default would build the console default keymap.
 
 For more information on the layout option and other ones, see the [`make` guide](https://docs.qmk.fm/getting_started_make_guide.html).
